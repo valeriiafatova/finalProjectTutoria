59c6c0cd62153f93a745c65c9973cfd9cf0849f4
Valeriia Fatova <valeriia.fatova@gmail.com>
2019-05-17 19:20:43 +0300

    04.3 add param

--- src/main/java/com/epam/web/HelloServlet.java
+++ src/main/java/com/epam/web/HelloServlet.java
@@ -5,10 +5,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("Hello from server");
+        String name = req.getParameter("name");
+        resp.getWriter().print("Hello from server "  + name);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        String name = request.getParameter("username");
+        String age = request.getParameter("userage");
+        String gender = request.getParameter("gender");
+        String country = request.getParameter("country");
+        String[] courses = request.getParameterValues("courses");
+
+        try {
+            writer.println("<p>Name: " + name + "</p>");
+            writer.println("<p>Age: " + age + "</p>");
+            writer.println("<p>Gender: " + gender + "</p>");
+            writer.println("<p>Country: " + country + "</p>");
+            writer.println("<h4>Courses</h4>");
+            for(String course: courses)
+                writer.println("<li>" + course + "</li>");
+        } finally {
+            writer.close();
+        }
+
     }
 }
